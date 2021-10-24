9a8f3fe0922ece9c831c44a989f24f687ee13208
Da-yun <dnfekfdl@o.cnu.ac.kr>
2021-10-24 21:38:28 +0900

    additem 수정

---
+++ src/components/Fonts/Font.js
@@ -0,0 +1,10 @@
+import * as Font from 'expo-font'; 
+import { AppLoading } from 'expo';
+
+
+const Fonts = () => Font.loadAsync({ 
+    JuaRegular: require('../../../assets/fonts/Jua-Regular.ttf'), 
+    KalamRegular: require('../../../assets/fonts/Kalam-Regular.ttf')
+ });
+   
+
