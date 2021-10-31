28d157f9b8ddbd53013172bd3541cf8f1cd5bb8a
Da-yun <dnfekfdl@o.cnu.ac.kr>
2021-10-31 17:11:22 +0900

    [#22] 회원가입 스크린, 홈스크린 수정

--- src/screens/SignUpScreen/SignUpScreen.js
+++ src/screens/SignUpScreen/SignUpScreen.js
@@ -1,13 +1,44 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { Text, View,TextInput, Keyboard ,TouchableWithoutFeedback,
-    TouchableOpacity } from 'react-native';
+    TouchableOpacity, Button, Image } from 'react-native';
 import GradientButton from "react-native-gradient-buttons";
 import { FontAwesome } from "react-native-vector-icons";
 import { Card } from 'react-native-paper';
 import styles from "./style"
 import { CommonActions } from "@react-navigation/routers";
-
+import * as ImagePicker from 'expo-image-picker';
+import { LinearGradient } from 'expo-linear-gradient';
+import MaskedView from '@react-native-masked-view/masked-view';
 function SignUpScreen({ navigation }){
+
+  const [image, setImage] = useState(null);
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ImagePicker.requestMediaLibraryPermissionsAsync();
+        if (status !== 'granted') {
+          alert('카메라 액세스를 허용해야 사진을 선택할 수 있습니다.');
+        }
+      }
+    })();
+  }, []);
+
+  const pickImage = async () => {
+    let result = await ImagePicker.launchImageLibraryAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      allowsEditing: true,
+      borderRadius: 100,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    console.log(result);
+
+    if (!result.cancelled) {
+      setImage(result.uri);
+    }
+  };
+
   return(
   <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
   <View style = {styles.screen}>
@@ -18,55 +49,49 @@ function SignUpScreen({ navigation }){
             style={styles.img}
           />
           <Text style={styles.headerText}>회원 등록</Text>
-      </View>
-          <View style={styles.all}>
-          <Card style={styles.content}>
+      </View> 
+        <Card style={styles.content}>
             <TextInput
-              textContentType="name"
-              placeholder="NAME"
+              placeholder="닉네임"
               placeholderTextColor="#707070"
               style={styles.input}
             />
             <TextInput
-              textContentType="emailAddress"
-              placeholder="EMAIL"
+              placeholder="내 소개글"
               placeholderTextColor="#707070"
               style={styles.input}
             />
-            <TextInput
-              textContentType="password"
-              secureTextEntry={true}
-              placeholder="PASSWORD"
-              placeholderTextColor="#707070"
-              style={styles.input}
-            />
-            <GradientButton
-              style={styles.gradient}
-              textStyle={styles.text}
-              text="SIGN UP"
-              onPressAction={() => {
-                navigation.dispatch(
+            <MaskedView
+            style={styles.masked}
+            maskElement={<Text style={styles.text}>프로필 사진을 선택해주세요</Text>}
+            >
+           <LinearGradient
+             colors={['cadetblue', '#ff3399']}
+             start={{ x: 1, y: 1 }}
+             end={{ x: 0, y: 2 }}
+             style={{ flex: 1 }}
+          />
+          </MaskedView>
+             {!image ?
+             <TouchableOpacity  onPress={pickImage}  style={styles.profile} >
+               <Text style={styles.text}>Click Me!</Text>
+             </TouchableOpacity> : 
+           <TouchableOpacity  onPress={pickImage} >
+           <Image onPress={pickImage} source={{ uri: image }} style={styles.profile}/>
+           </TouchableOpacity>}
+        </Card>
+        <GradientButton
+            style={styles.gradient}
+            textStyle={styles.text}
+            text="SIGN UP"
+            onPressAction={() => {
+              navigation.dispatch(
                   CommonActions.reset({ index: 1, routes: [{ name: "SignIn" }] })
-                );
-              }}
-              height={50}
-              violetPink
-              impact
-            />
-          </Card>
-        </View>
-           <View>
-          <View style={styles.textRow}>
-            <Text style={styles.leftText}>
-              이미 계정이 있으신가요?
-            </Text>
-            <TouchableOpacity onPress={() => {
-                navigation.dispatch(CommonActions.navigate({ name: "SignIn" }));
-              }}>
-            <Text style={styles.rigthText}>로그인하러가기</Text>
-            </TouchableOpacity>
-          </View>
-        </View>
+              );
+            }}
+            violetPink
+            impact
+        />
   </View>
  </TouchableWithoutFeedback>
   );
