3ade00253c30b5d1aa890902b3cf3467b41cd3d4
Da-yun <dnfekfdl@o.cnu.ac.kr>
2021-11-07 21:31:31 +0900

    [#23] public addItem 변경

--- src/components/AddButton/AddButton.js
+++ src/components/AddButton/AddButton.js
@@ -2,14 +2,10 @@ import React from 'react';
 import { TouchableOpacity, Text } from 'react-native';
 import styles from './style';
 
-function AddButton({ onPress, screenType }) {
+function AddButton({ onPress }) {
   return (
     <TouchableOpacity activeOpacity={0.8} style={styles.btn} onPress={onPress}>
-      {screenType == 'public' ? (
-        <Text style={styles.text}>다음</Text>
-      ) : (
-        <Text style={styles.text}>등록하기</Text>
-      )}
+      <Text style={styles.text}>등록하기</Text>
     </TouchableOpacity>
   );
 }
