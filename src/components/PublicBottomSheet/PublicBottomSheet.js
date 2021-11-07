04e194f464c9e575e1d791f4f55a8564463d07b7
Da-yun <ekdbs6963@naver.com>
2021-11-08 02:36:42 +0900

    [#32] public item 재구성

---
+++ src/components/PublicBottomSheet/PublicBottomSheet.js
@@ -0,0 +1,89 @@
+import React, { useEffect, useRef, useState } from 'react';
+import {
+  View,
+  Modal,
+  Animated,
+  TouchableWithoutFeedback,
+  Dimensions,
+  PanResponder,
+  TouchableOpacity,
+  FlatList,
+  Text,
+} from 'react-native';
+import styles from './style.js';
+
+const PublicBottomSheet = (props) => {
+  const { modalVisible, setModalVisible } = props;
+  const screenHeight = Dimensions.get('screen').height;
+  const panY = useRef(new Animated.Value(screenHeight)).current;
+
+  const translateY = panY.interpolate({
+    inputRange: [-1, 0, 1],
+    outputRange: [0, 0, 1],
+  });
+
+  const resetBottomSheet = Animated.timing(panY, {
+    toValue: 0,
+    duration: 300,
+    useNativeDriver: true,
+  });
+  const closeBottomSheet = Animated.timing(panY, {
+    toValue: screenHeight,
+    duration: 300,
+    useNativeDriver: true,
+  });
+  const closeModal = () => {
+    closeBottomSheet.start(() => {
+      setModalVisible(false);
+    });
+  };
+
+  const panResponders = useRef(
+    PanResponder.create({
+      onStartShouldSetPanResponder: () => true,
+      onMoveShouldSetPanResponder: () => false,
+      onPanResponderMove: (event, gestureState) => {
+        panY.setValue(gestureState.dy);
+      },
+      onPanResponderRelease: (event, gestureState) => {
+        if (gestureState.dy > 0 && gestureState.vy > 1.5) {
+          closeModal();
+        } else {
+          resetBottomSheet.start();
+        }
+      },
+    })
+  ).current;
+
+  useEffect(() => {
+    if (props.modalVisible) {
+      resetBottomSheet.start();
+    }
+  }, [props]);
+
+  return (
+    <Modal
+      visible={modalVisible}
+      animationType={'fade'}
+      transparent
+      statusBarTranslucent
+    >
+      <View style={styles.overlay}>
+        <TouchableWithoutFeedback onPress={closeModal}>
+          <View style={styles.background} />
+        </TouchableWithoutFeedback>
+        <Animated.View
+          style={{
+            ...styles.bottomSheetContainer,
+            transform: [{ translateY: translateY }],
+          }}
+          {...panResponders.panHandlers}
+        >
+          <Text>hi</Text>
+        </Animated.View>
+      </View>
+    </Modal>
+  );
+};
+
+export default PublicBottomSheet;
--- src/components/PublicItem/style.js
+++ src/components/PublicItem/style.js
@@ -1,6 +1,82 @@
 import { StyleSheet } from 'react-native';
+<<<<<<< HEAD
 import { windowHeight, windowWidth } from '../Dimentions/Dimentions';
 
 const styles = StyleSheet.create({});
+=======
+
+const styles = StyleSheet.create({
+  card: {
+    marginBottom: '2%',
+    backgroundColor: '#e5e2e9',
+    borderWidth: 2,
+    borderRadius: 5,
+    borderStyle: 'solid',
+    borderColor: '#8b00ff',
+  },
+  container: {
+    alignItems: 'center',
+    flex: 1,
+    height: '100%',
+    justifyContent: 'center',
+  },
+  title: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    color: '#fff',
+    justifyContent: 'flex-start',
+    alignItems: 'flex-start',
+  },
+  btn: {
+    position: 'absolute',
+    left: 50,
+  },
+  container1: {
+    flexDirection: 'row',
+    padding: 5,
+  },
+  title: {
+    width: '100%',
+    padding: 5,
+  },
+  container2: {
+    flex: 1,
+    padding: 15,
+  },
+  name: {
+    fontFamily: 'JuaRegular',
+    color: '#fff',
+    fontSize: 16,
+    padding: 8,
+  },
+  paragraph: {
+    marginTop: 0,
+    fontSize: 14,
+    fontWeight: 'bold',
+    textAlign: 'center',
+    fontFamily: 'JuaRegular',
+  },
+  text: {
+    marginTop: 5,
+    fontFamily: 'KalamRegular',
+  },
+  profile: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 30,
+    height: 30,
+    borderRadius: 100,
+    borderColor: '#8b00ff',
+    borderWidth: 2,
+  },
+  edit: {
+    width: '100%',
+    padding: 5,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+>>>>>>> 3d59f16 ([#32] public item 재구성)
 
 export default styles;
