034ea09caf5a4f143f45676745b0feba42621394
OMEGA-Y <science_co@naver.com>
2021-11-07 16:57:05 +0900

    [#28] profile screen UI

--- src/screens/PrivateScreen/PrivateScreen.js
+++ src/screens/PrivateScreen/PrivateScreen.js
@@ -1,15 +1,14 @@
 /* External dependencies */
-import React from 'react';
-import { View, FlatList } from 'react-native';
-import { Fab, Icon, Box, Center, NativeBaseProvider } from 'native-base';
-import { AntDesign } from '@expo/vector-icons';
-import { CommonActions } from '@react-navigation/routers';
+import React from "react";
+import { View, FlatList } from "react-native";
+import { Fab, Icon, Box, Center, NativeBaseProvider } from "native-base";
+import { AntDesign } from "@expo/vector-icons";
+import { CommonActions } from "@react-navigation/routers";
 
 /* Internal dependencies */
-import styles from './styles';
-
-import PrivateItem from '../../components/PrivateItem/PrivateItem';
-import usePrivate from '../../hooks/usePrivate';
+import styles from "./style";
+import PrivateItem from "../../components/PrivateItem/PrivateItem";
+import usePrivate from "../../hooks/usePrivate";
 
 function PrivateScreen({ navigation }) {
   const { privateArticleList, onCreate } = usePrivate({ folderId: 1 });
@@ -21,7 +20,7 @@ function PrivateScreen({ navigation }) {
         renderItem={({ item }) => (
           <PrivateItem
             text={item.content}
-            screen={'private'}
+            screen={"private"}
             navigation={navigation}
           />
         )}
@@ -43,10 +42,10 @@ function PrivateScreen({ navigation }) {
               onPress={() => {
                 navigation.dispatch(
                   CommonActions.navigate({
-                    name: 'AddItemStack',
+                    name: "AddItemStack",
                     params: {
-                      screenType: 'private',
-                      apiType: 'post',
+                      screenType: "private",
+                      apiType: "post",
                       onCreate: onCreate,
                     },
                   })
---
+++ src/screens/ProfileScreen/style.js
@@ -0,0 +1,22 @@
+import { StyleSheet } from "react-native";
+
+const styles = StyleSheet.create({
+  screen: {
+    alignItems: "center",
+    display: "flex",
+    height: "100%",
+    flexDirection: "column",
+    backgroundColor: "#F5F5F5",
+  },
+  input: {
+    borderWidth: 0.5,
+    borderColor: "#D3D3D3",
+    borderRadius: 50,
+    padding: 18,
+    marginVertical: 6,
+    fontWeight: "bold",
+    width: 300,
+  },
+});
+
+export default styles;
