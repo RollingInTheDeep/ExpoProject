bdd27b427687fbcf7142a9c439aafe0537b746a6
Da-yun <dnfekfdl@o.cnu.ac.kr>
2021-11-10 13:00:44 +0900

    [#34] public screen bugfix

--- src/screens/PublicScreen/style.js
+++ src/screens/PublicScreen/style.js
@@ -1,5 +1,5 @@
-import { StyleSheet } from 'react-native';
-
+import { StyleSheet, Dimensions } from 'react-native';
+export const { width, height } = Dimensions.get('window');
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -7,6 +7,9 @@ const styles = StyleSheet.create({
     padding: 8,
     backgroundColor: '#e5e2e9',
     opacity: 0.8,
+  },
+  storyContainer: {
+    flex: 1,
   },
   maskedView: {
     minHeight: 60,
@@ -17,9 +20,28 @@ const styles = StyleSheet.create({
     fontSize: 40,
     flex: 1,
   },
+  text: {
+    fontFamily: 'JuaRegular',
+    fontSize: 24,
+  },
   list: {
     minHeight: 60,
     width: '100%',
+    marginBottom: 10,
+  },
+  profile: {
+    alignSelf: 'center',
+    alignItems: 'center',
+    justifyContent: 'center',
+    width: 50,
+    height: 50,
+    marginHorizontal: 5,
+    borderRadius: 100,
+    borderColor: '#ed0086',
+    borderWidth: 4,
+  },
+  storyHeader: {
+    padding: 3,
   },
 });
 
