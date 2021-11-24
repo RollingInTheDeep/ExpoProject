50e0fd5b27f3132f0732eb26268bf0ef90374bbe
Da-yun <ekdbs6963@naver.com>
2021-11-25 01:10:16 +0900

    [#40] profile screen 작업

--- src/screens/ProfileScreen/ProfileScreen.js
+++ src/screens/ProfileScreen/ProfileScreen.js
@@ -1,5 +1,5 @@
 /* External dependencies */
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect } from 'react';
 import {
   Text,
   View,
@@ -7,25 +7,40 @@ import {
   TouchableOpacity,
   Image,
   FlatList,
-} from "react-native";
+} from 'react-native';
 
-import usePublicByUser from "../../hooks/usePublicByUser";
-import PublicItem from "../../components/PublicItem/PublicItem";
-
+import usePublicByUser from '../../hooks/usePublicByUser';
+import PublicItem from '../../components/PublicItem/PublicItem';
+import styles from './style';
+import { LinearGradient } from 'expo-linear-gradient';
 function ProfileScreen({ route, navigation }) {
   const userId = route.params.userId;
+  const image = route.params.img;
+  const nickName = route.params.nickName;
   const [modalVisible, setModalVisible] = useState(false);
   const { publicArticleList, onCreate } = usePublicByUser({ userId });
-
+  const count = publicArticleList.length;
   return (
-    <View>
-      <Text>hi</Text>
+    <View style={styles.container}>
+      <Image source={{ uri: image }} style={styles.profile} />
+      <LinearGradient
+        colors={['#e2b0ff', '#623aa2']}
+        start={{ x: 0.0, y: 1.0 }}
+        end={{ x: 1.0, y: 1.0 }}
+        style={styles.linear}
+      >
+        <TouchableOpacity style={styles.nickname}>
+          <Text style={styles.text}>{nickName}</Text>
+        </TouchableOpacity>
+      </LinearGradient>
+      <Text style={styles.count}>게시물</Text>
       <FlatList
+        style={styles.list}
         data={publicArticleList}
         renderItem={({ item }) => (
           <PublicItem
             item={item}
-            screen={"public"}
+            screen={'public'}
             navigation={navigation}
             setModalVisible={setModalVisible}
           />
